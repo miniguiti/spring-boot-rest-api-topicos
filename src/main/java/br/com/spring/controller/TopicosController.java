587fc1fade92aae490f29f8c86b742380cf545d4
miniguiti <gminiguiti@outlook.com>
2021-07-13 11:10:01 -0300

    DTO | Refatorando

--- src/main/java/br/com/spring/controller/TopicosController.java
+++ src/main/java/br/com/spring/controller/TopicosController.java
@@ -1,23 +1,27 @@
 package br.com.spring.controller;
 
+import br.com.spring.dto.TopicoDto;
 import br.com.spring.model.Curso;
 import br.com.spring.model.Topico;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+
+@RestController
 @Controller
 public class TopicosController {
 
     @RequestMapping("/topicos")
     @ResponseBody
-    public List<Topico> list(){
-         Topico topico = new Topico("Dúvida", "Duvida com Spring",
+    public List<TopicoDto> list(){
+         Topico topico = new Topico("Dúvida", "Dúvida com Spring",
                          new Curso("Spring", "Programação"));
 
-         return Arrays.asList(topico, topico, topico);
+         return TopicoDto.converter(Arrays.asList(topico, topico, topico));
     }
 }
