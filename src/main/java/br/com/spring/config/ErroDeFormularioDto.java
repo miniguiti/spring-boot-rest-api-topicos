a21bb34727e83dee969e3e6536e4452f917a6529
miniguiti <gminiguiti@outlook.com>
2021-07-21 10:54:10 -0300

    Controller advice com Dto de erro

---
+++ src/main/java/br/com/spring/config/ErroDeFormularioDto.java
@@ -0,0 +1,19 @@
+package br.com.spring.config;
+
+public class ErroDeFormularioDto {
+    private String campo;
+    private String messagem;
+
+    public ErroDeFormularioDto(String campo, String messagem) {
+        this.campo = campo;
+        this.messagem = messagem;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public String getMessagem() {
+        return messagem;
+    }
+}
